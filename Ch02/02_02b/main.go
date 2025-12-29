140f7bf6acf492345d107c0b7738955c8b637ed6
xanketevive <jordi.lopezp@gmail.com>
2025-12-29 19:53:35 +0000

    second  commit

--- Ch02/02_02b/main.go
+++ Ch02/02_02b/main.go
@@ -9,6 +9,13 @@ func main() {
 	 str1 := "The quick red fox"
 	 str2 := "jumped over"
 	 str3 := "the lazy brown dog."
+	 aNumber := 47
 
 	fmt.Println(str1, str2, str3)
+	strLenght, err := fmt.Println("The value is: ", aNumber)
+	if err == nil {
+		fmt.Println("String length:", strLenght)
+	}
+	fmt.Printf("Value of the number: %v\n", aNumber)
+	fmt.Printf("Data type: %T\n", aNumber)
 }
--- Ch02/02_03b/main.go
+++ Ch02/02_03b/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	str, _ := reader.ReadString('\n')
+	fmt.Println(str)
+
+	fmt.Print("Enter a nubmer: ")
+	str, _ = reader.ReadString('\n')
+	f, err := strconv.ParseFloat(strings.TrimSpace(str),64)
+	if err != nil {
+		fmt.Println(err)		
+	} else {
+		fmt.Println("Value of number:", f)
+	}
 
 }
